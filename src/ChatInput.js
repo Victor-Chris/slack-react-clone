690e64d3a4e5cd0b43b5132e89e5a3b67c80ed26
Victor Chris <victorchris51@gmail.com>
2020-09-01 22:11:22 +0300

    Fixed issue of error popping up while posting to database

--- src/ChatInput.js
+++ src/ChatInput.js
@@ -3,19 +3,18 @@ import firebase from 'firebase';
 import './ChatInput.css';
 import db from './firebase';
 import { useStateValue } from './StateProvider';
-import { Input } from '@material-ui/core';
 
 function ChatInput({ channelName, channelId }) {
     const [input, setInput] = useState('');
     const [{ user }] = useStateValue();
 
-    const sendMessage = e =>{
+    const sendMessage = (e) =>{
         e.preventDefault();
 
         if(channelId){
             db.collection('rooms').doc(channelId).collection('messages').add({
                 message: input,
-                timestamp: firebase.firestore.FieldValue.serverTimestamp,
+                timestamp: firebase.firestore.FieldValue.serverTimestamp(),
                 user: user.displayName,
                 UserImage: user.photoURL
             })
@@ -29,7 +28,7 @@ function ChatInput({ channelName, channelId }) {
             <form>
                 <input 
                     value={input}
-                    onChange={e => setInput()}
+                    onChange={e => setInput(e.target.value)}
                     placeholder={`Message #${channelName}`}
                 />
                 <button type="submit" onClick={sendMessage}>Send</button>
